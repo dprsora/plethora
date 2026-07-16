2b933c901d29db86025f6c0e8b638dad4d4502fe
sophie <sophie.harshini.raja@gmail.com>
2026-07-16 11:45:23 -0500

    Initial Commit

---
+++ plethora/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  plethora
+//
+//  Created by DPI Student 011 on 7/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ plethora/plethoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  plethoraApp.swift
+//  plethora
+//
+//  Created by DPI Student 011 on 7/16/26.
+//
+
+import SwiftUI
+
+@main
+struct plethoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
